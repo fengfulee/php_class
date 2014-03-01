227033504564580b9a8b66d839e2cb807069ca96
= <=>
2014-03-01 20:03:38 +0800

    一个不错的调用类方法的例子

--- fengfulee.Good.class.php
+++ fengfulee.Good.class.php
@@ -1,3 +1,26 @@
 <?PHP
+#	php 中一个不错的类的调用方法..很适合一些不用返回值的函数.
+class Good{
+	var $name;
+	var $sex;
+#	给$name初始化..
+	function name($name){
+		$this->name = $name;
+		return $this;
+	}
+#	给性别初始化..
+	function sex($sex){
+		$this->sex = $sex;
+		return $this;
+	}
+#	打印...
+	function show(){
+		print "Name:{$this->name};Sex:{$this->sex}";
+	}
+	
+}//end of clazz.
+
+$good = new Good();
+$good->name('fengfulee')->sex('男')->show();
 
 ?>
