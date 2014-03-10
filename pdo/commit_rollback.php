cf1cf572140adb7e90c3cc13ab358ab16ec455e3
= <=>
2014-03-10 09:22:30 +0800

    添加了一个实现的PDO事务.

--- pdo/commit_rollback.php
+++ pdo/commit_rollback.php
@@ -1,3 +1,27 @@
 <?PHP
+	#这里的一个实例是讲解如何使用PDO的事务处理,以及异常类的使用..
+	$dsn = "mysql:host=localhost;dbname=test";
+	$username = 'root';
+	$password = 'root';
+try{
+	$db = new PDO($dsn,$username,$password);
+	$db->query('set name uft8');
+	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$db->beginTransaction();	#开始事务处理
+	$db->exec('insert into `test`.`user` (`name`,`age`) values ("fengfulee","23");');
+	$db->exec('insert into `test`.`user` (`name`,`age`) values ("fengfulee","23");');
+	$db->exec('insert into `test`.`user` (`name`,`age`) values ("fengfulee","23");');
+	$db->exec('insert into `test`.`user` (`name`,`age`) values ("fengfulee","23");');
+	$db->exec('insert into `test`.`user` (`name`,`age`) values ("fengfulee","23");');
+	#这里要注意下,我没有sex这个字段.那么也就会报错,这样就执行事务的回滚操作...
+	$db->exec('insert into `test`.`user` (`name`,`sex`) values ("fengfulee","男");');
+	$db->commit();	#提交事务处理.
+}catch(Exception $e){
+	$db->rollBack();			#执行回滚操作
+	echo "FAILED:".$e->getMessage();
+}
 
+#################test
+#这里会报一个错误. column `age` not found in table `user`....
+#同样前面的操作不能执行到数据库当中....
 ?>
--- php_xml.php
+++ php_xml.php
@@ -1,3 +1,33 @@
 <?PHP
+#	用PHP 的DOM控件来创建XML输出...
+#	设置输出内容的类型为xml..
+	header('Content-type:text/xml;');
+	$dom = new DOMDocument('1.0','utf-8');
 
+	//建立response标签..
+	$response = $dom -> createElement('response');
+	$dom -> appendChild($response);
+	
+	$books = $dom -> createElement('books');
+	$response -> appendChild($books);
+	
+	$title = $dom -> createElement('title');
+	$titleText = $dom -> createTextNode('PHP与AJAX');
+	$title -> appendChild($titleText);
+
+	$isbn  = $dom -> createElement('isbn');
+	$isbnText = $dom -> createTextNode('123456');
+	$isbn -> appendChild($isbnText);
+	
+	$book = $dom -> createElement('book');
+	$book -> appendChild($title);
+	$book -> appendChild($isbn);
+
+	$books -> appendChild($book);
+	#$books -> appendChild($book);
+	#要进行保存..
+	$xmlString = $dom -> saveXML();
+
+	echo $xmlString;
+	
 ?>
